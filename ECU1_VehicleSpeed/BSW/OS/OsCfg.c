ffc98ddf83000093caaafae02b324febc386efd3
baobluebird <lebaohv1236bqt@gmail.com>
2025-03-04 15:58:13 +0700

    update OS file keil uvision

---
+++ ECU1_VehicleSpeed/BSW/OS/OsCfg.c
@@ -0,0 +1,130 @@
+/*******************************************************************************
+	Module Name:		OsCfg.c
+	Generation Date:	2025-March-Tuesday	14:46:51
+	Tool Version: 		V.0
+	Description: 		Os configuration src file
+
+
+	All Rights Reserved - Sarea Alhariri
+*******************************************************************************/
+
+/******************************************************************************/
+/*******************AUTO GENERATED FILE PLEASE DO NOT TOUCH********************/
+/******************************************************************************/
+
+#include "OsCfg.h"
+
+#define		INVALID_ID		0xFFU
+#define		NULL		(void *)0
+
+/*******************************************************************************
+	Num of app tasks
+*******************************************************************************/
+const uint8_t OsCfg_MAX_NUM_OF_TASKS = 	4U;
+
+/*******************************************************************************
+	Num of system alarms
+*******************************************************************************/
+const uint8_t OsCfg_MAX_NUM_OF_ALARMS = 1U;
+/*******************************************************************************
+	Num of system resources
+*******************************************************************************/
+const uint8_t OsCfg_MAX_NUM_OF_RESOURCES = 0U;
+/*******************************************************************************
+	App task externs
+*******************************************************************************/
+const uint16_t OsCfg_ALL_STACK_SIZE = 1792;
+/*******************************************************************************
+	Hook routines configurations
+*******************************************************************************/
+const uint8_t OsCfg_USE_ERROR_HOOK = 0U;
+const uint8_t OsCfg_USE_PRETASK_HOOK = 0U;
+const uint8_t OsCfg_USE_POSTTASK_HOOK = 0U;
+const uint8_t OsCfg_USE_STARTUP_HOOK = 0U;
+const uint8_t OsCfg_USE_STACK_OVER_FLOW_HOOK = 0U;
+
+
+const uint8_t OsCfg_USE_PARAMETER_ACCESS = 0U;
+const uint8_t OsCfg_USE_GET_SERVICE_ID = 0U;
+const uint32_t OsCfg_SYSTEM_TICK_IN_MS = 10U;
+const uint32_t OSTICKDURATION = 10000U;
+/*******************************************************************************
+	Task contol block table
+*******************************************************************************/
+tcb_t OsCfg_TCBs[4/*num of app tasks*/+1/*for Idle mechanism */] = 
+{
+	{/*SP*/0u, /*basic SP*/0u , /*task pointer*/OsTask_NVM_Logging_Task, 
+	/*task state*/SUSPENDED, /*DeadBeefLoc*/NULL, /*priority*/4, /*task model*/EXTENDED,
+	/*set events*/0u, /*wait events*/0u, /*res occupation*/0u, 
+	/*preemptability*/1u, /*schedule requested */0u},
+
+	{/*SP*/0u, /*basic SP*/0u , /*task pointer*/OsTask_DEM_Task, 
+	/*task state*/SUSPENDED, /*DeadBeefLoc*/NULL, /*priority*/4, /*task model*/EXTENDED,
+	/*set events*/0u, /*wait events*/0u, /*res occupation*/0u, 
+	/*preemptability*/1u, /*schedule requested */0u},
+
+	{/*SP*/0u, /*basic SP*/0u , /*task pointer*/OsTask_Can_Send_Task, 
+	/*task state*/SUSPENDED, /*DeadBeefLoc*/NULL, /*priority*/4, /*task model*/EXTENDED,
+	/*set events*/0u, /*wait events*/0u, /*res occupation*/0u, 
+	/*preemptability*/1u, /*schedule requested */0u},
+
+	{/*SP*/0u, /*basic SP*/0u , /*task pointer*/OsTask_Sensor_Read_Task, 
+	/*task state*/READY, /*DeadBeefLoc*/NULL, /*priority*/3, /*task model*/BASIC,
+	/*set events*/0u, /*wait events*/0u, /*res occupation*/0u, 
+	/*preemptability*/0u, /*schedule requested */0u},
+
+	{0u, 0u, OsTask_IdleMechanism,     READY, NULL, 255u, BASIC, 0u, 0u,
+	 0u, 1u, 0u }
+};
+
+/*******************************************************************************
+	Stack Size For Each Thread
+*******************************************************************************/
+const uint32_t OsCfg_StackSize[4] = 
+{
+	512,		/*NVM_Logging_Task*/
+	256,		/*DEM_Task*/
+	512,		/*Can_Send_Task*/
+	512		/*Sensor_Read_Task*/
+};
+
+/*******************************************************************************
+	Stack Buffer Allocation For All Threads
+*******************************************************************************/
+uint32_t OsCfg_Stack[1792+16/* 16 for Idle mechanism */] = {0u};
+
+/*******************************************************************************
+	Alarms base records to be referenced in alarm configs
+*******************************************************************************/
+AlarmBaseType MainCounter = { 0xFFFFFFFF, 1, 1};
+
+/*******************************************************************************
+	Alarm control blocks table
+*******************************************************************************/
+acb_t OsCfg_Alarms[1/*OsCfg_MAX_NUM_OF_ALARMS*/] =
+{
+	{/*fire time*/100, /*cyclic time*/10, /*call back*/NULL,
+	/*action*/ALARM_ACTION_TASK,
+	/*base type*/&MainCounter, /*task id*/Sensor_Read_Task, /*event id*/INVALID_ID,
+	/*enable status*/true}		/*Alarm ID: Alarm_Periodic*/
+};
+/*******************************************************************************
+	Resource task authorization
+*******************************************************************************/
+const uint8_t OsCfg_ResourceTaskAuthorized[1][1]; /*Not used*/
+/*******************************************************************************
+	Ceiling priority for system resources
+*******************************************************************************/
+const uint8_t OsCfg_ResourceCeilPri[1]; /*Not used*/
+/*******************************************************************************
+	Error Code task buffer
+*******************************************************************************/
+StatusType SVCCnxt_ErrCodes[4/*OsCfg_MAX_NUM_OF_TASKS*/] = {E_OK};
+/*******************************************************************************
+	SVC resource context
+*******************************************************************************/
+ResourceType SVCCnxt_ResMgrCnxt[1];/*Not used*/
+/*******************************************************************************
+	Resource control block table
+*******************************************************************************/
+RCB_t RCB[1]; /*Not used*/
---
+++ ECU1_VehicleSpeed/BSW/OS/OsekOSECU1.c
@@ -0,0 +1,142 @@
+#include "os.h"
+#include "Rte_SpeedSensor.h"
+#include "Rte_Can.h"
+
+static uint8_t SensorData_Toggle = 0U;
+static uint8_t CanSend_Toggle = 0U;
+static uint8_t DemSend_Toggle = 0U;
+static uint8_t NvmLogging_Toggle = 0U;
+
+volatile uint8_t SensorTask_Running = 0;
+volatile uint8_t CanTask_Running = 0;
+volatile uint8_t DEMTask_Running = 0;
+volatile uint8_t NVMLoggingTask_Running = 0;
+
+volatile uint8_t Check_Data_Read = 0;
+volatile uint8_t Check_Data_Send = 0;
+volatile uint8_t Check_Dem_Send = 0;
+volatile uint8_t Check_Nvm_Stored = 0;
+
+VAR(uint16_t, AUTOMATIC) speed;
+
+DeclareTask(Sensor_Read_Task);
+DeclareTask(Can_Send_Task);
+DeclareTask(DEM_Task);
+DeclareTask(NVM_Logging_Task);
+
+void SystemInit(void) {}
+
+int main(void)
+{
+	  
+    StartOS();
+    while (1);
+    return 0;
+}
+
+TASK(Sensor_Read_Task)
+{		
+	  //SensorTask_Running = 1; //check time task Can_Send_Task
+		SensorData_Toggle ^= 1;
+	  VAR(Std_ReturnType, AUTOMATIC) status;
+		status = Rte_Call_RP_SpeedSensorSWC_ReadSpeed(&speed);
+
+		if (status == RTE_E_OK)
+		{
+				Check_Data_Read = 1;
+			  ActivateTask(Can_Send_Task);
+        SetEvent(Can_Send_Task, SpeedSendEvt);
+		}
+		else
+		{
+				Check_Data_Read = 0;
+				ActivateTask(DEM_Task);
+        SetEvent(DEM_Task, DemReqEvt);
+		}
+
+	  
+	 // SensorTask_Running = 0;//check time task Sensor_Read_Task
+	
+	  TerminateTask(); 
+	  
+}
+
+TASK(Can_Send_Task)
+{
+    //CanTask_Running = 1;//check time task Can_Send_Task
+	
+    WaitEvent(SpeedSendEvt); 
+    ClearEvent(SpeedSendEvt); 
+	
+		CanSend_Toggle ^= 1; 
+	
+	  VAR(Std_ReturnType, AUTOMATIC) status;
+	  status = Rte_Write_CANMessage(speed);
+    
+		if (status == RTE_E_OK)
+		{
+				Check_Data_Send = 1;
+
+		}
+		else
+		{
+				Check_Data_Send = 0;
+		}
+	  
+	  //CanTask_Running = 0;//check time task Can_Send_Task
+	
+	  TerminateTask(); 
+	
+}
+
+TASK(DEM_Task)
+{
+    //DEMTask_Running = 1;//check time task DEM_Task
+	
+    WaitEvent(DemReqEvt); 
+    ClearEvent(DemReqEvt); 
+    DemSend_Toggle ^= 1; 
+	  VAR(Std_ReturnType, AUTOMATIC) status;
+	  status = Rte_Call_SpeedSensorSWC_StoreErrorToNVM();
+	
+	  if (status == RTE_E_OK)
+		{
+				Check_Nvm_Stored = 1;
+			  ActivateTask(NVM_Logging_Task);
+        SetEvent(NVM_Logging_Task, NvmReqEvt);
+
+		}
+		else
+		{
+				Check_Nvm_Stored = 0;
+		}
+	  //DEMTask_Running = 0;//check time task DEM_Task
+	
+	  TerminateTask(); 
+	
+}
+
+TASK(NVM_Logging_Task)
+{
+    //NVMLoggingTask_Running = 1;//check time task NVM_Logging_Task
+	
+    WaitEvent(NvmReqEvt); 
+    ClearEvent(NvmReqEvt); 
+    NvmLogging_Toggle ^= 1; 
+	  VAR(Std_ReturnType, AUTOMATIC) status;
+	  status = Rte_Call_SpeedSensorSWC_CheckAndReportError();
+	
+	  if (status == RTE_E_OK)
+		{
+				Check_Dem_Send = 1;
+
+		}
+		else
+		{
+				Check_Dem_Send = 0;
+		}
+	  //NVMLoggingTask_Running = 0;//check time task NVM_Logging_Task
+	
+	  TerminateTask(); 
+	
+}
