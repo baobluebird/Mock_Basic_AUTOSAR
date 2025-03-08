ed9982e2cb5a5f653541ad861dc733355e30d983
baobluebird <lebaohv1236bqt@gmail.com>
2025-03-08 20:37:27 +0700

    update wgd

---
+++ ECU2_FIControl/BSW/Services/Wdg/WdgM.c
@@ -0,0 +1,36 @@
+#include "WdgM.h"
+#include <stdint.h>
+
+extern volatile uint8_t Simulate_Error;  
+
+void WdgM_Init(void)
+{
+    WdgIf_Init();
+}
+
+WdgM_StatusType WdgM_CheckStatus(void)
+{
+    if (Simulate_Error == 1)  
+    {
+        return WDGM_SYSTEM_ERROR;
+    }
+    Simulate_Error = 0;    
+    return WDGM_SYSTEM_OK;
+}
+
+
+void WdgM_MainFunction(void)
+{
+    static uint8_t system_healthy = 1; 
+
+    if (system_healthy)
+    {
+        WdgIf_Trigger();
+    }
+    else
+    {
+        //printf("System Error: Watchdog not triggered!\n");
+    }
+
+    WdgIf_CheckTimeout();
+}
